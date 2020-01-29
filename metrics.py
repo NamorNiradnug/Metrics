f0eb3085347588e42de9000ae5dbc5946c148af2
NamorNiradnug <roma937a@mail.ru>
2020-01-29 22:02:10 +0300

    add files

---
+++ metrics.py
@@ -0,0 +1,33 @@
+"""Copyright (c) 2020 by Roman Gundarin (NamorNiradnug)
+
+This is module with functions of different metrics, such as Euclidean metric,
+discrete metric and Hamming distance and ect."""
+
+
+def _get_default(data: object, index: int, default: object = 0) -> object:
+    """Get iterible object 'data' and return 'data['i']' if it is possible else return 'default'."""
+    if len(data) - 1 < index:
+        return default
+    return data[index]
+
+
+def euclidean_metric(point1: tuple, point2: tuple) -> float:
+    """Get two points in Euclidean space and calculate distanse between them."""
+    return sum([_get_default(point1, i) ** 2 - _get_default(point2, i) ** 2
+                for i in range(len)]) ** .5
+
+
+def discrete_metric(object1: object, object2: object) -> int:
+    """Discrete metrics between objects is 1 if 'object1' == 'object2' else return 0."""
+    return int(object1 == object2)
+
+
+def hamming_distance(string1: object, string2: object) -> int:
+    """Hamming distance between iterible objects 'string1' and 'string2' is
+    number of positions 'i' that 'string1['i']' is not equel 'string2['i']'."""
+    counter = 0
+    len1 = len(string1)
+    len2 = len(string2)
+    for i in range(min(len1, len2)):
+        counter += not string1[i] == string2[i]
+    return counter + abs(len1 - len2)
