04c570f363126dbcd83657ecd407cde5ab63070f
NamorNiradnug <roma937a@mail.ru>
2020-01-30 17:31:48 +0300

    good start page

--- web_metrics.py
+++ web_metrics.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello, World! I\'m Namor Niradnug'
+@app.route('/', methods=['POST', 'GET'])
+def start() -> 'html':
+    return render_template('start.html',
+                           the_title='metrics',
+                           distance='')
 
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
