8980fceb56daf7d0d691c15e435e4cd5bc4d6600
NamorNiradnug <50048443+NamorNiradnug@users.noreply.github.com>
2020-02-05 18:35:14 +0300

    add metrics

--- metrics.py
+++ metrics.py
@@ -11,18 +11,27 @@ def _get_default(data: object, index: int, default: object = 0) -> object:
     return data[index]
 
 
-def euclidean_metric(point1: tuple, point2: tuple) -> float:
+def euclideanMetric(point1: (float, ...), point2: (float, ...)) -> float:
     """Get two points in Euclidean space and calculate distanse between them."""
     return sum([_get_default(point1, i) ** 2 - _get_default(point2, i) ** 2
                 for i in range(len)]) ** .5
 
 
-def discrete_metric(object1: object, object2: object) -> int:
+def discreteMetric(object1: object, object2: object) -> int:
     """Discrete metrics between objects is 1 if 'object1' == 'object2' else return 0."""
     return int(object1 == object2)
 
 
-def hamming_distance(string1: object, string2: object) -> int:
+def manhattenMetric(point1: (float, ...), point2: (float, ...)) -> float:
+    """Manhattan distance (or City Block distance) equel sum of abs(point1[i] - point2[i]) for all i."""
+    return sum([abs(_get_default(point1, i, 0) - _get_default(point2, i, 0)) for i in range(max(len(point1), len(point2)))])
+
+
+def chebyshevDistance(point1: (float, ...), point2: (float, ...)) -> float:
+    """Chebyshev distance bettween points is the greatest of their differences along any coordinate dimension."""
+
+
+def hammingDistance(string1: object, string2: object) -> int:
     """Hamming distance between iterible objects 'string1' and 'string2' is
     number of positions 'i' that 'string1['i']' is not equel 'string2['i']'."""
     counter = 0
